cee80f99c2886783417a01ae4ba4c9aed59c24fa
Thomas Nibler <dev@tnibler.de>
2020-07-04 17:13:25 +0200

    Fixed build and updated deps

--- complete_build.sh
+++ complete_build.sh
@@ -3,7 +3,6 @@
 git submodule init 
 git submodule update
 
-cd ..
 if [ -f "ffmpeg-android-maker/output/lib/armeabi-v7a/libavformat.so" -a -f "ffmpeg-android-maker/output/lib/armeabi-v7a/libavutil.so" -a -f "ffmpeg-android-maker/output/lib/armeabi-v7a/libavcodec.so" -a -f "ffmpeg-android-maker/output/lib/armeabi-v7a/libswscale.so" ]; then
 	echo "ffmpeg already built"
 else
